748d8b1b077d96f7ab339ee859231978ab3e5447
Treldo <17614761838@163.com>
2019-09-22 23:54:30 +0800

    Initial commit

---
+++ lisp/init-packages.el
@@ -0,0 +1,60 @@
+;;  __               __                       __
+;; |   \    /\     /    `   |   /    /\     /    \   ___
+;; |___/   /__\   |         |--|    /__\   |   ~~   |___
+;; |      /    \   \_ _ /   |   \  /    \   \ __ /  |___
+;;                                   __
+;; |\  /|    /\    |\  |    /\     /    \    ___  |\  /|   ___  |\  |  _ _ _
+;; | \/ |   /__\   | \ |   /__\   |   ~~    |___  | \/ |  |___  | \ |    |
+;; |    |  /    \  |  \|  /    \   \ __ /   |___  |    |  |___  |  \|    |
+
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			   ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"))))
+
+;; cl - Command Lisp Extension
+;; It can be replace by cl-loop for loop
+(require 'cl)
+
+;; Add Packages
+(defvar my/packages '(
+		      company
+		      smartparens
+		      hungry-delete
+		      swiper
+		      counsel
+		      popwin
+		      ) "Default packages")
+
+(setq package-selected-packages my/packages)
+
+(defun my/packages-installed-p ()
+  (loop for pkg in my/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+
+(unless (my/packages-installed-p)
+  (message "%s" "Refreshing package datebase...")
+  (package-refresh-contents)
+  (dolist (pkg my/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+;; Find Executable Path on OS X
+;;(when (memq window-system '(mac ns))
+;;  (exec-path-from-shell-initialize))
+
+(global-company-mode 1)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+
+;; load custom themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+
+(require 'popwin)
+(popwin-mode t)
+
+(provide 'init-packages)
