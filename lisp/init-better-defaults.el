680530e4654612d0f8bed144f8656e6ec36f4453
Treldo <17614761838@163.com>
2019-09-23 22:32:04 +0800

    indent-buffer

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -18,4 +18,25 @@
 
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
+;; Auto format
+(defun indent-buffer ()
+  "Indent the currently visited buffer."
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun indent-region-or-buffer ()
+  "Indent a region if selected, otherwise the whole buffer."
+  (interactive)
+  (save-excursion
+    ;; If the part was selected, try to format it.
+    (if (region-active-p)
+	(progn
+	  (indent-region (region-beginning) (region-end))
+	  (message "Indented selected region."))
+      (progn
+	(indent-buffer)
+	(message "Indent buffer.")))))
+
+(global-set-key (kbd "C-M-\\") 'indent-region-or-buffer)
+
 (provide 'init-better-defaults)
