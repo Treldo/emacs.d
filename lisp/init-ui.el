214bb9d24bc10da4e75e59c17742942632804f91
Treldo <17614761838@163.com>
2019-09-24 15:15:34 +0800

    Nyan cat

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -15,4 +15,30 @@
 ;;(setq default-frame-alist
 ;;      '((height . 70) (width . 120) (menu-bar-lines . 20) (tool-bar-lines . 0)))
 
+;; Nyan cat
+(add-to-list 'load-path "~/.emacs.d/custom-mode/nyan-mode")
+(require 'nyan-mode)
+(setq default-mode-line-format (list ""
+				     'mode-line-modified
+				     "<"
+				     "Vubee"
+				     "> "
+				     "%10b"
+				     '(:eval (when nyan-mode (list (nyan-create))))
+				     " "
+				     'default-directory
+				     " "
+				     "%[("
+				     'mode-name
+				     'minor-mode-list
+				     "%n"
+				     'mode-line-process
+				     ")%]--"
+				     "Line %l--"
+				     '(-3 . "%P")
+				     "-%-"))
+(nyan-mode t)
+(nyan-start-animation)
+(setq-default nyan-wavy-trail t)
+
 (provide 'init-ui)
