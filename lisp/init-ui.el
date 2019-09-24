2a97625a7b46d5a2b1cffcce3585f34ea209c7fe
Treldo <17614761838@163.com>
2019-09-24 09:51:41 +0800

    screen-maxized

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -5,14 +5,14 @@
 (setq inhibit-splash-screen t)
 
 ;; screen maximized
-;;(setq initial-frame-alist (quote ((fullscreen . maximized))))
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
 
 (set-default-font "Monaco-14")
 
 (set-face-attribute 'default nil :height 150)
 
-(set-frame-position (selected-frame) 550 0)
-(setq default-frame-alist
-      '((height . 70) (width . 120) (menu-bar-lines . 20) (tool-bar-lines . 0)))
+;;(set-frame-position (selected-frame) 550 0)
+;;(setq default-frame-alist
+;;      '((height . 70) (width . 120) (menu-bar-lines . 20) (tool-bar-lines . 0)))
 
 (provide 'init-ui)
